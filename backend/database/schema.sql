2112c07d289e9a73050f68b83237ec0ee5a64978
alain le moal <wolfdu28@aol.com>
2023-12-01 11:59:27 +0100

    modi database

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,3 +1,10 @@
+create table marque (
+  id int primary key auto_increment not null,
+  nom VARCHAR(200) not NULL
+);
+
+INSERT INTO marque(nom) VALUES ('addidas'),('nike');
+
 create table kimono (
   id int unsigned primary key auto_increment not null,
   style varchar(255) not null,
@@ -5,10 +12,10 @@ create table kimono (
   marque_id INT not NULL,
   FOREIGN KEY (marque_id) REFERENCES marque(id)
 );
+INSERT INTO kimono(style,taille,marque_id) VALUES ('kata', 100, 1);
 
-create table marque (
-  id int unsigned primary key auto_increment not null,
-  nom VARCHAR(200) not NULL,
-);
+
+
+
 
 
